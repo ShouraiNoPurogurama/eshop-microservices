f4fcbd740285fd6d22b0c03644e9fb90e2fdfa93
NA <nhatanhtruong20@gmail.com>
2024-11-16 21:59:17 +0700

    feat: abstract ICommand and IQuery from IRequest

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommand : ICommand<Unit>;
+
+// Covariance allows assignments of derived type of TResponse to TResponse
+//Which means: ICommand<TResponse> res = null; ICommand<TResponseSuperClass> sup = res is allowed
+public interface ICommand<out TResponse> 
+    : IRequest<TResponse> where TResponse : notnull;
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommandHandler<in TCommand, TResponse>
+    : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse> //ICommand is more generic than TCommand so that we should use the contravariance keyword
+    where TResponse : notnull
+;
